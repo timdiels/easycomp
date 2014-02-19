cce58f21f64238c8ab0d901127b59492511417e9
timdiels <timdiels.m@gmail.com>
2014-02-19 17:32:15 +0100

    Added general project structure

---
+++ src/easycomp/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 by Tim Diels
+ *
+ * This file is part of easycomp.
+ *
+ * easycomp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * easycomp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with easycomp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Note: if you change a file, add your name to the copyright in top header
+
+#include <iostream>
+
+using namespace std;
+
+namespace EASYCOMP {
+
+int main(int argc, char** argv) {
+    cout << "this might be used as a start for tests, or perhaps the cmake needs to be adjusted to produce a so/dll instead and each test compiled separately to use this" << endl;
+    return 0;
+}
+
+}
